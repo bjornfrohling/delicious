a590be3c3569f6a31c777c72e4bfd62a8929aa53
bjorn <bjorn.frohling@gmail.com>
2022-10-05 23:48:59 +0300

    Initial SEO optimization

---
+++ components/SeoPage.js
@@ -0,0 +1,12 @@
+import { NextSeo } from 'next-seo';
+
+const Page = () => (
+    <>
+        <NextSeo
+            title="Delicious"
+            description="The adjective 'delicious' is used to describe a situation or activity that gives you great pleasure:"
+        />
+    </>
+);
+
+export default Page;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,37 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import TypeWriter from "../components/TypeWriter";
+import {NextSeo} from "next-seo";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Delicious</title>
-        <meta name="Delicious" content="Welcome to a delicious Life!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <TypeWriter/>
-      </main>
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <NextSeo
+                title="Delicious"
+                description="The adjective 'delicious' is used to describe a situation or activity that gives you great pleasure:"
+                defaultTitle="Delicious"
+                openGraph={{
+                    url: "https://www.delicious.ee/",
+                    title: "Delicious",
+                    description:
+                        "The adjective 'delicious' is used to describe a situation or activity that gives you great pleasure:"
+                }}
+                additionalMetaTags={[
+                    {
+                        property: "keywords",
+                        content:
+                            "delicious, meaning of delicious, delightful, delectable"
+                    },
+                ]}
+            />
+            <Head>
+                <title>Delicious</title>
+                <meta name="Delicious" content="The meaning of delicious"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <main className={styles.main}>
+                <TypeWriter/>
+            </main>
+        </div>
+    )
 }
