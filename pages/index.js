53560f6dde7eb4fed5562200f787bdae0732d0b8
bjorn <bjorn.frohling@gmail.com>
2022-11-05 17:52:39 +0200

    seo and head tag re-arrangement

--- pages/index.js
+++ pages/index.js
@@ -5,45 +5,50 @@ import {NextSeo} from "next-seo";
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <>
             <NextSeo
-                title="delicious"
-                description="A minimalistic mantra. The adjective 'delicious' is used to describe a situation or activity that gives you great pleasure:"
-                defaultTitle="delicious"
+                title="delicious.ee"
+                description="Welocme to delicious.ee - a minimalistic mantra. Highly pleasant to the taste. Old French delicious, delicieux, from Late Latin dēliciōsus "
+                defaultTitle="delicious.ee"
                 openGraph={{
                     url: "https://www.delicious.ee/",
-                    title: "delicious",
+                    title: "delicious.ee",
                     description:
-                        "A minimalistic mantra. The adjective 'delicious' is used to describe a situation or activity that gives you great pleasure:"
+                        "Welocme to delicious.ee - a minimalistic mantra. Highly pleasant to the taste. Old French delicious, delicieux, from Late Latin dēliciōsus "
                 }}
                 additionalMetaTags={[
                     {
                         property: "keywords",
                         content:
-                            "mantra, minimalism, delicious, meaning of delicious, delightful, delectable, life"
+                            "mantra, minimalism, delicious, meaning of delicious, delightful, delectable, life, delicious, delicieux, dēliciōsus"
                     },
                 ]}
             />
+
             <Head>
                 <title>delicious</title>
                 <meta name="delicious.ee"
-                      content="The meaning of delicious. A daily minimalistic mantra."
+                      content="The meaning of delicious. A minimalistic daily mantra."
                       description="A minimalistic mantra. The adjective 'delicious' is used to describe a situation or activity that gives you great pleasure:"
                 />
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <main className={styles.main}>
-                <TypeWriter/>
-            </main>
-            <footer className={styles.footer}>
-                <a
-                    href="https://frohling.se"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    designed by frohling.se
-                </a>
-            </footer>
-        </div>
+
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    <TypeWriter/>
+                </main>
+                <footer className={styles.footer}>
+                    <a
+                        href="https://frohling.se"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        designed by frohling.se
+                    </a>
+                </footer>
+            </div>
+        </>
+
     )
 }
