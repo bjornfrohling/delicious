176c778fef1fdd454e5772cd6d6d933d0295beae
Björn Fröhling <bjoern.froehling@gmail.com>
2025-06-20 19:29:20 +0300

    Added Sitemap script

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// filepath: /Users/bjornatwork/w/delicious/next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://www.delicious.ee',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
